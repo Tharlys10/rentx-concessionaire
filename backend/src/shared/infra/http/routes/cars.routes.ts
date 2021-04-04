a2fb0b211fd790a2e5942b9cad122331bbaf7fe9
tharlys10 <tharlys10c@hotmail.com>
2021-04-04 00:19:21 -0300

    feat: :sparkles: implementing the module to list all available cars
    implementando o modulo para lista todos os carros disponiveis

--- backend/src/shared/infra/http/routes/cars.routes.ts
+++ backend/src/shared/infra/http/routes/cars.routes.ts
@@ -1,14 +1,18 @@
 import { Router } from "express";
 
-import { CreateCarController } from "@modules/cars/useCases/createCar/CreateCarController";
-
 import { ensureAuthentication } from "../middlewares/ensureAuthentication";
 import { ensureAdmin } from "../middlewares/ensureAdmin";
+
+import { CreateCarController } from "@modules/cars/useCases/createCar/CreateCarController";
+import { ListAvailableCarsController } from "@modules/cars/useCases/listAvailableCars/ListAvailableCarsController";
+
 
 const carsRoutes = Router();
 
 const createCarController = new CreateCarController();
+const listAvailableCarsController = new ListAvailableCarsController();
 
 carsRoutes.post("/", ensureAuthentication, ensureAdmin, createCarController.handle)
+carsRoutes.get("/available", listAvailableCarsController.handle)
 
 export { carsRoutes }
