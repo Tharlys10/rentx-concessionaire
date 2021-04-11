a03d2cd8ac74c3a9796633583363d093eca31f02
tharlys10 <tharlys10c@hotmail.com>
2021-04-11 18:48:34 -0300

    feat: :sparkles: implemented the functions to refresh token
    implementado as funções para atualizar token

---
+++ backend/src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: "d5e80b8fdcb061b0ff78dec00a992757",
+  expires_in_token: "15m",
+  secret_refresh_token: "d20f769c0944b4e3b84613b244297e3a",
+  expires_in_refresh_token: "30d",
+  expires_refresh_token_days: 30
+}
---
+++ backend/src/modules/accounts/dtos/ICreateUserTokenDTO.ts
@@ -0,0 +1,8 @@
+
+interface ICreateUserTokenDTO {
+  user_id: string;
+  expires_date: Date;
+  refresh_token: string;
+}
+
+export { ICreateUserTokenDTO }
