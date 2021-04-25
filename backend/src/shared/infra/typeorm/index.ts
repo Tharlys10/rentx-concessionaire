7c4564bf8c4807cbfb087513572b1a4ae8e4bfdf
tharlys10 <tharlys10c@hotmail.com>
2021-04-25 00:10:06 -0300

    feat: :sparkles: update docker

--- backend/src/shared/infra/typeorm/index.ts
+++ backend/src/shared/infra/typeorm/index.ts
@@ -4,12 +4,12 @@ interface IOptions {
   host: string;
 }
 
-export default async (host = "localhost"): Promise<Connection> => {
+export default async (host = "rentxdatabase"): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
 
   return createConnection(
     Object.assign(defaultOptions, {
-      host: process.env.NODE_ENV === 'test' ? 'localhost' : host,
+      host: process.env.NODE_ENV === 'test' ? 'rentxdatabase' : host,
       database: process.env.NODE_ENV === 'test' ? 'rentx_test' : defaultOptions.database
     })
   )
