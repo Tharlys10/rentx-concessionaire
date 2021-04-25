af1e794eff2ec956ae4517ba129d21474fad448c
tharlys10 <tharlys10c@hotmail.com>
2021-04-25 01:16:32 -0300

    feat: :sparkles: update routes

--- backend/src/shared/infra/http/app.ts
+++ backend/src/shared/infra/http/app.ts
@@ -23,14 +23,14 @@ const app = express();
 
 app.use(express.json());
 
+app.use("/rentxserver/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+app.use("/rentxserver/avatar", express.static(`${upload.tmpFolder}/avatar`));
+app.use("/rentxserver/cars", express.static(`${upload.tmpFolder}/cars`));
+
 app.use(cors());
 
 app.use("/rentxserver", router);
-
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
-
-app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
-app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 
 // errors
 app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
