884ac727026825f6a3e016e627fe7fdfe9837aa9
tharlys10 <tharlys10c@hotmail.com>
2021-04-25 00:26:34 -0300

    feat: :sparkles: cors

--- backend/src/shared/infra/http/app.ts
+++ backend/src/shared/infra/http/app.ts
@@ -23,12 +23,13 @@ const app = express();
 
 app.use(express.json());
 
+app.use(cors());
+
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
 app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 
-app.use(cors())
 
 app.use(router);
 
